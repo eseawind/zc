c36f757838583584ee42d03da60904452e191263
xiaoymin <xiaoymin@foxmail.com>
2014-07-22 20:55:55 +0800

    spring配置

--- zc/src/main/java/com/zcnation/zc/action/IndexAction.java
+++ zc/src/main/java/com/zcnation/zc/action/IndexAction.java
@@ -1,5 +1,13 @@
 package com.zcnation.zc.action;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexAction {
+	@RequestMapping("/index.html")
+	public String to_index(){
+		return "index";
+	}
 
 }
